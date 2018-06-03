1cb594429d917c86db5aac0949b0f74f75ac188f
CalbazaAlex <alex_calbaza97@yahoo.com>
2018-06-03 03:53:33 +0300

    Alex project

--- src/test/java/TestAddDonation.java
+++ src/test/java/TestAddDonation.java
@@ -1,12 +1,31 @@
 import junit.framework.TestCase;
+import org.springframework.context.ApplicationContext;
+import service.LoginService;
+import service.TCPService;
+import utils.CommonUtils;
 
 public class TestAddDonation extends TestCase {
+    private TCPService service;
+    private ApplicationContext context;
+    private Integer numberOfDonations;
+    private LoginService login;
     public void setUp(){
-        //cod pregatitor
+        this.context=CommonUtils.getFactory();
+        this.service = this.context.getBean(TCPService.class);
+        this.login=this.context.getBean(LoginService.class);
+        login.handleLogin("tcp","tcp");
+        this.numberOfDonations=service.handleGetDonations().size();
     }
 
     public void testUp(){
-        //assert-uri
-        assertEquals(1,1);
+        assertNotSame("Success",service.handleAddDonation("dasdas","In curs de validare","435.28","This is a test"));
+        assertEquals(numberOfDonations.intValue(),service.handleGetDonations().size());
+        assertEquals("Success",service.handleAddDonation("21321312123","In curs de validare","435.28","This is a test"));
+        assertEquals(numberOfDonations+1,service.handleGetDonations().size());
+
+    }
+
+    public void tearDown(){
+
     }
 }
